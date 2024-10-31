3ecf7e69b08a0c05ae940b9420b367c1f27395f4
Dino298 <105893868+Dino298@users.noreply.github.com>
2024-10-30 19:59:44 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function agregarAlCarrito(nombreProducto, precio) {
+    alert(`Agregaste ${nombreProducto} al carrito por ${precio.toLocaleString('es-CO', { style: 'currency', currency: 'COP' })}.`);
+    // Aquí puedes agregar más lógica para manejar el carrito
+}
